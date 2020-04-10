16e685c48e10716f1ec74794ed8df59957122082
Rashed <Rashed@DESKTOP-SHB33BJ>
2020-04-10 20:07:24 +0600

    Initial

---
+++ LemdaForums.Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using LemdaForums.Models;
+using LemdaForums.Data;
+
+namespace LemdaForums.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+
+        public  DbSet<ApplicationUser> ApplicationUsers { get; set; }
+
+    }
+}
---
+++ LemdaForums/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LemdaForums.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
